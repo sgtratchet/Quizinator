137d5824cdc676ea55f0ea8fc504e9c3df25097d
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-05-25 21:18:55 -0600

    got the questions to show up and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,47 @@ var choiceB = document.getElementById("choiceB");
 var choiceC = document.getElementById("choiceC");
 var choiceD = document.getElementById("choiceD");
 var answerButton = document.getElementById('scoring-container')
-
+//array of questions
+var questionEl= [
+    {
+        question: 'who created javascript?', 
+        answers: { 
+            choiceA: "Douglas Crockford",
+            choiceB: "Sheryl Sandberg",
+            choiceC: "Brendan Eich",
+            choiceD: "Jennifer Anniston"
+            },
+        correctAnswer: "choiceC"   
+    },
+        {question: 'what is a variable in javascript?', 
+        answers: {
+            choiceA:"a method of storing information",
+            choiceB:"something to represent unknown information",
+            choiceC:"type of headache",
+            choiceD:"Eva Mendes",
+        },
+        correctAnswer: "choiceA"
+        
+    },
+        {question: 'which is NOT a way to store information using javascript?', 
+        answers: {       
+            choiceA: "Using an Array",
+            choiceB: "Local Storage",
+            choiceC: "Scarlett Johansson",
+            choiceD: "Session Storage",
+        },
+        correctAnswer:"choiceC"
+    },
+        {question: 'which of the following will make something disappear?', 
+        answers: {      
+            choiceA: "window.prompt",
+            choiceB: "Gerard Butler",
+            choiceC: "function(erase)",
+            choiceD: "(CSS) display:none.",
+        },
+        correctAnswer:"choiceD"
+    }
+];
 var start = confirm("Push the button, Max!");
     if (start){
         quizStart();
@@ -19,50 +59,18 @@ var start = confirm("Push the button, Max!");
         start();
     };
 
-
-//array of questions
-var questionEl= [
-    {question: 'who created javascript?', 
-            choiceA:"Douglas Crockford",
-            choiceB: "Sheryl Sandberg",
-            choiceC: "Brendan Eich",
-            choiceD: "Jennifer Anniston",
-            correctAnswer:"choiceC"
-    },
-    {question: 'what is a variable in javascript?', 
-            choiceA:"a method of storing information",
-            choiceB:"something to represent unknown information",
-            choiceC:"type of headache",
-            choiceD:"Eva Mendes",
-            correctAnswer:"choiceA"
-    },
-    {question: 'which is NOT a way to store information using javascript?', 
-            choiceA: "Using an Array",
-            choiceB: "Local Storage",
-            choiceC: "Scarlett Johansson",
-            choiceD: "Session Storage",
-            correctAnswer:"choiceC"
-    },
-    {question: 'which of the following will make something disapear?', 
-            choiceA: "window.prompt",
-            choiceB: "Gerard Butler",
-            choiceC: "function(erase)",
-            choiceD: "(CSS) display:none.",
-            correctAnswer:"choiceD"
-    }
-];
 var questionIndex = 0;
 console.log(questionEl[0].question);
 function quizStart(){
         timer();
     //question loop
-    for (var i = 0; i <questionEl.length;i++){
-        var q= questionEl[questionIndex];
-        questionContainer.innerHTML = "<h2>" + questionEl+ "</h2>";
-        choiceA.innerHTML = q.choiceA;
-        choiceB.innerHTML = q.choiceB;
-        choiceC.innerHTML = q.choiceC;
-        choiceD.innerHTML = q.choiceD;
+    for (var i = 0; i < questionEl.length;i++){
+        var q = questionEl[i];
+        quiz.innerHTML = "<h2>" + questionEl[i].question + "</h2>";
+        choiceA.innerHTML = q.answers.choiceA;
+        choiceB.innerHTML = q.answers.choiceB;
+        choiceC.innerHTML = q.answers.choiceC;
+        choiceD.innerHTML = q.answers.choiceD;
 
         if(answerButton === q.correctAnswer)
         {score++};
@@ -86,4 +94,5 @@ function timer(){
         };
     }, 1000);
 };
-answerButton.addEventListener('click", showResults');
+
+answerButton.addEventListener("click", showResults);
